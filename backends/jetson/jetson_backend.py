Fixed grpc model broadcast

backends/jetson/jetson_backend.py
@@ -7,8 +7,19 @@ from ..backends import BaseBackend
 class JetsonBackend(BaseBackend):
     def pack_model(self, model: nn.Module):
         packed_model = io.BytesIO()
-        torch.jit.script(model, packed_model)
-        return packed_model
+        jit_model = torch.jit.script(model)
+        torch.jit.save(jit_model, packed_model)
+        packed_model.seek(0)
+        return packed_model.getvalue()
 
-    def unpack_model(self, packed_model: io.BytesIO):
+    def unpack_model(self, packed_model_bytes: io.BytesIO):
+        packed_model = io.BytesIO(packed_model_bytes)
         return torch.jit.load(packed_model)
+
+    def download_model(self, packed_model_bytes: io.BytesIO):
+        model_buffer = io.BytesIO(packed_model_bytes)
+        model_buffer.seek(0)
+        model = torch.jit.load(model_buffer)
+        torch.jit.save(model, 'model.pt')
+
+        print(model(torch.randn(10)))

networking/grpc/server.py
@@ -1,14 +1,14 @@
 import grpc
 from concurrent import futures
 from networking.grpc import node_service_pb2
-import node_service_pb2_grpc
+from networking.grpc import node_service_pb2_grpc
 import torch
+from orchestration.node import Node
 
 
 class GRPCServer(node_service_pb2_grpc.NodeServiceServicer):
-    def __init__(self):
-        # self.host = host
-        # self.port = port
+    def __init__(self, node: Node):
+        self.node = node
         self.server = None
 
     def start(self):
@@ -16,7 +16,7 @@ class GRPCServer(node_service_pb2_grpc.NodeServiceServicer):
         node_service_pb2_grpc.add_NodeServiceServicer_to_server(self, self.server)
         self.server.add_insecure_port('[::]:50051')
         self.server.start()
-        print(f"Server started on port {self.port}.")
+        print("Server started on port 50051.")
         self.server.wait_for_termination()
 
     def stop(self):
@@ -25,7 +25,8 @@ class GRPCServer(node_service_pb2_grpc.NodeServiceServicer):
             self.server.wait_for_termination()
 
     def SendModel(self, request, context):
-        torch.jit.save(request.model_file, 'model.pt')
+        print("Received model bytes:", len(request.model_file))
+        self.node.download_model(request.model_file)
         return node_service_pb2.Empty()
         
     def SendCheckpoint(self, request, context):
@@ -33,9 +34,6 @@ class GRPCServer(node_service_pb2_grpc.NodeServiceServicer):
         tensor_data = None
         result = None
         return node_service_pb2.Tensor(tensor_data=tensor_data, shape=result.shape, dtype=str(result.dtype)) if result is not None else node_service_pb2.Tensor()
-
-    def SayHello(self, request, context):
-        return node_service_pb2.HelloReply(message=f"Hello, {request.name}!")
 
 
 if __name__ == "__main__":

test_client.py
@@ -2,6 +2,8 @@ import torch.nn as nn
 from networking.grpc.client import GRPCNode
 from backends.jetson.jetson_backend import JetsonBackend
 
+from distributed import 
+
 
 class SimpleModel(nn.Module):
     def __init__(self):
@@ -18,9 +20,12 @@ class SimpleModel(nn.Module):
 
 
 backend = JetsonBackend()
-packed_model = backend.pack_model(SimpleModel())
+model = SimpleModel()
+packed_model = backend.pack_model(model)
 
-node = GRPCNode('192.168.1.101')
+
+# node = GRPCNode('192.168.1.101')
+node = GRPCNode('localhost')
 node.connect()
-
-node.say_hello()
+node.upload_model(packed_model)
+# node.say_hello()